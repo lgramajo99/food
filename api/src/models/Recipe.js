09b4ffd9081818c107edc4d103eee3b803222530
lgramajo99 <lucianongramajo@gmail.com>
2023-04-13 00:21:06 -0300

    se crean los modelos de la BD

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('recipe', {
+    ID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    resumenDelPlato: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    nivelComidaSaludable: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    pasoAPaso: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  });
+};
