b9446b3047eae089a6a1c27ffa05597d844a5e1e
lgramajo99 <lucianongramajo@gmail.com>
2023-04-17 03:16:37 -0300

    se agreganunos componentes requeridos,

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,14 @@
+import { } from './action-types';
+
+const initialState = {
+
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+
+        default: return state;
+    }
+}
+
+export default reducer;
